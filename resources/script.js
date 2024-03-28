4613fa3ae6e5425c29edd714b06368471bb333f1
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2024-03-28 14:48:56 +1000

    Add div structure and initial CSS

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,24 @@
 let gridContainer = document.getElementById('gridContainer');
 const squares = [];
 
-for (i = 1; i <= 256; i++){
+for (i = 0; i <= 255; i++){  // create divs
     squares[i] = document.createElement('div');
     gridContainer.appendChild(squares[i]);
 }
+
+// TEST
+// for (i = 0; i <= 255; i++){
+//     squares[i].textContent = i;
+// }
+// END test
+
+squares.forEach((square) => {
+    square.classList.add('square');
+});
+
+squares.forEach((square) => {
+    square.addEventListener(onclick, () => {
+        square.setAttribute("style", "background-color: pink;");
+    });
+});
 
