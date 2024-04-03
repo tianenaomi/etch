ef4a2c063df72a2ffdc89084e32c0302fc958d28
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2024-04-03 10:11:55 +1000

    Add pop up box and further CSS

--- README.md
+++ README.md
@@ -4,4 +4,40 @@ Project: Etch-a-Sketch, Foundations Course on The Odin Project
 
 Objective: To build a browser version of an etch-a-sketch game to practice and imbed DOM manipulation learnings. 
 
+Checklist:
+1. Follow our instructions on setting up your project’s GitHub repository. COMPLETE
+
+2. Create a webpage with a 16x16 grid of square divs. COMPLETE
+
+3. Create the divs using JavaScript. Don’t try to create them by hand by copying and pasting them in your HTML file! COMPLETE
+
+4. It’s best to put your grid squares inside a “container” div. This div can be written in your HTML file. COMPLETE
+
+5. Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name, do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path. COMPLETE
+
+6. Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. COMPLETE
+
+7. Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed, and a new grid should be generated in the same total space as before (e.g., 960px wide) so that you’ve got a new sketch pad.
+
+Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+
+Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
+
+Also check out prompts.
+
+You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
+
+8. Push your project to GitHub!
+
+EXTRA CREDIT
+Transform the behavior of a square when interacting with the mouse by introducing a series of modifications.
+
+1. Rather than squares being the same color throughout the grid, randomize the squares’ RGB values with each interaction.
+
+2. Additionally, implement a progressive darkening effect where each interaction darkens the square by 10%. The objective is to achieve a completely black square only ten interactions.
+
+Hint: The opacity CSS property is useful here. To learn how to use it, check this MDN docs article about the opacity CSS property.
+You can choose to do either one or both of these challenges, it’s up to you.
+
+
 -->
--- resources/script.js
+++ resources/script.js
@@ -1,13 +1,32 @@
-let gridContainer = document.getElementById('gridContainer');
+const gridContainer = document.getElementById('gridContainer');
+const clear = document.getElementById('clear'); 
+const openPopUp = document.getElementById('newGrid');
+const popUp = document.querySelector('.popUp')
+const closePopUp = document.getElementById('submit')
+const input = document.getElementById('input');
+const error = document.getElementById('error');
 const squares = [];
 
-for (i = 0; i <= 255; i++){  // create divs
-    squares[i] = document.createElement('div');
-    gridContainer.appendChild(squares[i]);
+createGrid();
+
+function createGrid (input = "1000"){
+    for (i = 0; i <= input; i++){
+        squares[i] = document.createElement('div');
+        gridContainer.appendChild(squares[i]);
+    }
 }
 
+// function createGrid (input = "15"){
+//     input = +input;
+//     input *= input;
+//     for (i = 0; i <= input; i++){
+//         squares[i] = document.createElement('div');
+//         gridContainer.appendChild(squares[i]);
+//     }
+// }
+
 // TEST
-// for (i = 0; i <= 255; i++){
+// for (i = 0; i <= 1000; i++){
 //     squares[i].textContent = i;
 // }
 // END test
@@ -17,8 +36,52 @@ squares.forEach((square) => {
 });
 
 squares.forEach((square) => {
-    square.addEventListener(onclick, () => {
-        square.setAttribute("style", "background-color: pink;");
+    square.addEventListener('mouseenter', () => {
+        square.setAttribute("style", "background-color: #fb02af;");
     });
 });
+
+clear.addEventListener('click', () => {
+    squares.forEach((square) => {
+        square.setAttribute("style", "background-color: #012932;");
+    });
+});
+
+openPopUp.addEventListener('click', () => {
+    popUp.classList.add('open');
+});
+
+
+/* PSEUDOCODE
+ADD event listener on click on closePopUp
+    IF input is greater than 100 or less than 10 THEN
+        ADD error message
+        TRY input again (how to code this bit)
+    ELSE 
+        REMOVE current grid
+        ADD new grid using input
+        REMOVE class of open
+    END if then else
+END event listner
+*/
+
+
+
+closePopUp.addEventListener('click', () => { //set new grid
+    if (input > 100 || input < 10){
+        error.textContent = "must be between 10 and 100";
+        input.focus();
+    } else {
+        for (i = 0; i <= 255; i++){
+            squares[i].remove();
+        };
+        input = input * input;
+        createGrid();
+        popUp.classList.remove('open');
+    }
+    
+});
+
+
+
 
