ea75ab68114d0fd4acb0faca1b1115a7bb8d57d2
Tiane Zerba <148304153+tianenaomi@users.noreply.github.com>
2024-04-18 15:21:16 +1000

    Add custom colour HTML and updated JS variables

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,8 @@
 const gridContainer = document.getElementById('gridContainer');
 const clear = document.getElementById('clear'); 
-const openPopUp = document.getElementById('newGrid');
-const popUp = document.querySelector('.popUp');
-const closePopUp = document.getElementById('submit');
+const openNewGrid = document.getElementById('newGrid');
+const gridPopUp = document.getElementById('gridPopUp');
+const closeNewGrid = document.getElementById('submit');
 const error = document.getElementById('error');
 let sideSquares = document.getElementById('input');
 let input = document.getElementById('input');
@@ -46,21 +46,27 @@ clear.addEventListener('click', () => {
     });
 });
 
-openPopUp.addEventListener('click', () => {
-    popUp.classList.add('open');
+openNewGrid.addEventListener('click', () => {
+    gridPopUp.classList.add('open');
     input.focus();
+    input.value = null;
 });
 
-closePopUp.addEventListener('click', function(e) {
+closeNewGrid.addEventListener('click', function(e) {
     e.preventDefault(); //prevents browser from returning to line 1 and re-executing code
     let newValue = input.value;
-    sideSquares = +newValue;
+    if (newValue === null || newValue === undefined || newValue <= 16){
+        newValue = 16;
+    } else if (newValue >= 208){
+        newValue = 208;
+    };
+    sideSquares = newValue;
     for (let i = 0; i < squares.length; i++){
         squares[i].remove();
     };
     createGrid(sideSquares);
     setSquareSize(newValue);
-    popUp.classList.remove('open');
+    gridPopUp.classList.remove('open');
 });
 
 function setSquareSize (newValue){
@@ -71,8 +77,14 @@ function setSquareSize (newValue){
     });
 }
 
+// CUSTOM COLOURS BELOW
+let 
 
-/* PSEUDOCODE - how to resize gridContainer to fit window?
+
+/* PSEUDOCODE - additional features
+    1. how to resize grid to fit window while keeping aspect at 1:1.
+    2. add custom colours
+    3. pop up closes when user clicks anywhere other than close button
 
 */
 
